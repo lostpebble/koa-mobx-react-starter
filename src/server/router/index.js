4447893600af69949dd30ad83382d5bb733b01e6
Paul Myburgh <paulmyburgh88@gmail.com>
2016-09-27 16:14:23 +0200

    fixed server router so that user and counter api are always called at both routes (injecting both responses into the mobx stores for server rendering)

--- src/server/router/index.js
+++ src/server/router/index.js
@@ -2,7 +2,7 @@ import makeRouter from 'koa-router';
 
 const router = makeRouter();
 
-router.get('/', async(ctx, next) => {
+router.get('/*', async(ctx, next) => {
   console.log("Setting counter random value and getting a random user profile");
 
   await ctx.state.mobx.CounterStore.setRandomNumber();
